c4e46cdb722476f7603ea13d9d330d0b28ca452a
Matteo Rovellini <teorove04@gmail.com>
2023-05-30 11:54:31 +0200

    Changed activity_secondary.xml and added input bet

--- app/src/main/java/it/matteorovellini/fanta_assenze/SecondaryActivity.java
+++ app/src/main/java/it/matteorovellini/fanta_assenze/SecondaryActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +17,9 @@ import java.util.ArrayList;
 public class SecondaryActivity extends AppCompatActivity {
 
     ArrayList<TextView> textViews=new ArrayList<>();
-    Double potential_win=2.0;
+    Double potential_win=1.0;
+    Double bet=2.0;
+    Double total_stake=1.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +28,39 @@ public class SecondaryActivity extends AppCompatActivity {
 
         LinearLayout scroll_layout=(LinearLayout) findViewById(R.id.scroll_layout2);
         TextView textView_potential_win=(TextView) findViewById(R.id.textView_potential_win);
+        EditText editText_bet=(EditText) findViewById(R.id.editTextNumber_bet);
+        editText_bet.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    bet=Double.parseDouble(editText_bet.getText().toString());
+                    potential_win=bet*total_stake;
+                    textView_potential_win.setText(String.format("%.2f", potential_win)+" €");
+                } catch (Exception e) {
+                    //editText_bet.setText("2");
+                    //potential_win=2.0;
+                }
+            }
+        });
 
         int i=0;
         while(i<MainActivity.list.size()) {
             create_textView(scroll_layout, i);
-            potential_win=potential_win*getStake(MainActivity.list.get(i));
-            textView_potential_win.setText(String.format("%.2f", potential_win)+" €");
+            total_stake*=getStake(MainActivity.list.get(i));
             i++;
         }
+
+        potential_win=Double.parseDouble(editText_bet.getText().toString())*total_stake;
+        textView_potential_win.setText(String.format("%.2f", potential_win)+" €");
     }
 
     private void create_textView(LinearLayout layout, int i) {
